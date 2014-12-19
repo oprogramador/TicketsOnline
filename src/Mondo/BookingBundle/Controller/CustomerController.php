1900e1850681faa6d2e4a95545e45c81537d6028
oprogramador <uomodislesia@gmail.com>
2014-12-19 03:58:09 +0100

    ruouting update...

--- src/Mondo/BookingBundle/Controller/CustomerController.php
+++ src/Mondo/BookingBundle/Controller/CustomerController.php
@@ -41,37 +41,26 @@ class CustomerController extends Controller implements ITranslateable
     }
 
 
-
-
-
-    /**
-     * Lists all Customer entities.
-     *
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entities = $em->getRepository('MondoBookingBundle:Customer')->findAll();
-
-        return $this->render('MondoBookingBundle:Customer:index.html.twig', array(
-            'entities' => $entities,
-        ));
-    }
-
     private function sendMail($to, $vernr) {
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Hello Email')
-            ->setFrom('send@example.com')
-            ->setTo($to)
-            ->setBody(
-                $this->renderView(
-                    'MondoBookingBundle:Mail:email.txt.twig',
-                    array('vernr' => $vernr)
+        file_put_contents('/home/pierre/log.txt', "\n\nmail", FILE_APPEND);
+        try {
+            file_put_contents('/home/pierre/log.txt', "\n\ntry", FILE_APPEND);
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Ticket booking confirmation')
+                ->setFrom('send@example.com')
+                ->setTo($to)
+                ->setBody(
+                    $this->renderView(
+                        'MondoBookingBundle:Mail:email.txt.twig',
+                        array('vernr' => $vernr)
+                    )
                 )
-            )
-            ;
-        $this->get('mailer')->send($message);
+                ;
+            $this->get('mailer')->send($message);
+        } catch(\Exception $e) {
+            file_put_contents('/home/pierre/log.txt', "\n\ncatch", FILE_APPEND);
+            return $this->redirect($this->generateUrl('customer_new'));
+        }
     }
 
     /**
@@ -92,7 +81,7 @@ class CustomerController extends Controller implements ITranslateable
             $em->flush();
 
             $this->sendMail($entity->getEmail(), $entity->getVernr());
-            return $this->redirect($this->generateUrl('customer_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('customer_show', array('vernr' => $entity->getVernr())));
         }
 
         return $this->render('MondoBookingBundle:Customer:new.html.twig', array(
@@ -115,7 +104,7 @@ class CustomerController extends Controller implements ITranslateable
             'method' => 'POST',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+        $form->add('submit', 'submit', array('label' => $this->get('translator')->trans('create')));
 
         return $form;
     }
@@ -139,135 +128,21 @@ class CustomerController extends Controller implements ITranslateable
      * Finds and displays a Customer entity.
      *
      */
-    public function showAction($id)
+    public function showAction($vernr)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('MondoBookingBundle:Customer')->find($id);
+        $entity = $em->createQuery('SELECT x FROM Mondo\BookingBundle\Entity\Customer x WHERE x.vernr=:nr')
+            ->setParameter('nr', $vernr)
+            ->getSingleResult();
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Customer entity.');
         }
-
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('MondoBookingBundle:Customer:show.html.twig', array(
             'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
         ));
-    }
-
-    /**
-     * Displays a form to edit an existing Customer entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MondoBookingBundle:Customer')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Customer entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('MondoBookingBundle:Customer:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Creates a form to edit a Customer entity.
-     *
-     * @param Customer $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createEditForm(Customer $entity)
-    {
-        $form = $this->createForm(new CustomerType(), $entity, array(
-            'action' => $this->generateUrl('customer_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Customer entity.
-     *
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MondoBookingBundle:Customer')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Customer entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('customer_edit', array('id' => $id)));
-        }
-
-        return $this->render('MondoBookingBundle:Customer:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-    /**
-     * Deletes a Customer entity.
-     *
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('MondoBookingBundle:Customer')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Customer entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('customer'));
-    }
-
-    /**
-     * Creates a form to delete a Customer entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('customer_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-            ;
     }
 
     public function verifyAction(Request $request, $vernr)
